3d33a542558520780542ac7d5c2197f147413a7d
Jkutkut <jorgeregonzalez@gmail.com>
2022-09-27 13:51:50 +0200

    implementing visualizer

---
+++ processplanifiervisualizer.py
@@ -0,0 +1,13 @@
+class ProcessPlanifierVisualizer:
+    COLORS = {
+        '\033[0;31m', # Red
+        '\033[0;32m', # Green
+        '\033[0;33m', # Yellow
+        '\033[0;34m', # Blue
+        '\033[0;35m', # Purple
+        '\033[0;36m', # Cyan
+        '\033[0;37m'  # White
+    }
+
+    
+
