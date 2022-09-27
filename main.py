81206a3fd4ff8774d452ee23697ff0cd6da04d6c
Jkutkut <jorgeregonzalez@gmail.com>
2022-09-27 13:51:38 +0200

    main working with modules

---
+++ main.py
@@ -0,0 +1,13 @@
+from AsciiGraph import *
+
+
+if __name__ == "__main__":
+    plots = [
+        {
+            "values": [i for i in range(0, 10)],
+        }
+    ]
+    keys = [i for i in range(0, 10)]
+
+    graph = AsciiGraph.plot(plots, keys);
+    print(graph)
