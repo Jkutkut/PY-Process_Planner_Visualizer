678dd7a85d35856b38859ab2434619908d14181d
Jkutkut <jorgeregonzalez@gmail.com>
2022-09-28 13:59:50 +0200

    simulation by priority implemented

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ from processplanifiervisualizer import ProcessPlanifierVisualizer
 
 
 if __name__ == "__main__":
-    # TODO
+    ps_priority = [
+        Process("P1", priority = 1, t_cpu = 1),
+        Process("P2", priority = 1, t_cpu = 2),
+        Process("P3", priority = 6, t_cpu = 3),
+        Process("P4", priority = 7, t_cpu = 5),
+        Process("P5", priority = 8, t_cpu = 4),
+        Process("P6", priority = 9, t_cpu = 3),
+        Process("P7", priority = 9, t_cpu = 1),
+        Process("P8", priority = 9, t_cpu = 1),
+    ]
+
     ps1 = [
         Process("P1", t_arrival = 1, t_cpu = 1),
         Process("P2", t_arrival = 1, t_cpu = 2),
@@ -16,68 +26,8 @@ if __name__ == "__main__":
         Process("P8", t_arrival = 9, t_cpu = 1),
     ]
 
-    ps2 = [
-        Process("P1",  t_arrival = 0, t_cpu = 1),
-        Process("P2",  t_arrival = 1, t_cpu = 2),
-        Process("P3",  t_arrival = 1, t_cpu = 3),
-        Process("P4",  t_arrival = 1, t_cpu = 1),
-        Process("P5",  t_arrival = 1, t_cpu = 2),
-        Process("P6",  t_arrival = 1, t_cpu = 1),
-        Process("P7",  t_arrival = 1, t_cpu = 1),
-        Process("P8",  t_arrival = 1, t_cpu = 2),
-        Process("P9",  t_arrival = 1, t_cpu = 1),
-        Process("P10", t_arrival = 1, t_cpu = 1),
-        Process("P11", t_arrival = 1, t_cpu = 3),
-        Process("P12", t_arrival = 1, t_cpu = 1),
-        Process("P13", t_arrival = 1, t_cpu = 1),
-        Process("P14", t_arrival = 1, t_cpu = 1),
-        Process("P15", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-    ]
-    ps3 = [
-        Process("P1",  t_arrival = 0, t_cpu = 1),
-        Process("P2",  t_arrival = 1, t_cpu = 2),
-        Process("P3",  t_arrival = 1, t_cpu = 3),
-        Process("P4",  t_arrival = 1, t_cpu = 1),
-        Process("P5",  t_arrival = 1, t_cpu = 2),
-        Process("P6",  t_arrival = 1, t_cpu = 1),
-        Process("P7",  t_arrival = 1, t_cpu = 1),
-        Process("P8",  t_arrival = 1, t_cpu = 2),
-        Process("P9",  t_arrival = 1, t_cpu = 1),
-        Process("P10", t_arrival = 1, t_cpu = 1),
-        Process("P11", t_arrival = 1, t_cpu = 3),
-        Process("P12", t_arrival = 1, t_cpu = 1),
-        Process("P13", t_arrival = 1, t_cpu = 1),
-        Process("P14", t_arrival = 1, t_cpu = 1),
-        Process("P15", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P1",  t_arrival = 0, t_cpu = 1),
-        Process("P2",  t_arrival = 1, t_cpu = 2),
-        Process("P3",  t_arrival = 1, t_cpu = 3),
-        Process("P4",  t_arrival = 1, t_cpu = 1),
-        Process("P5",  t_arrival = 1, t_cpu = 2),
-        Process("P6",  t_arrival = 1, t_cpu = 1),
-        Process("P7",  t_arrival = 1, t_cpu = 1),
-        Process("P8",  t_arrival = 1, t_cpu = 2),
-        Process("P9",  t_arrival = 1, t_cpu = 1),
-        Process("P10", t_arrival = 1, t_cpu = 1),
-        Process("P11", t_arrival = 1, t_cpu = 3),
-        Process("P12", t_arrival = 1, t_cpu = 1),
-        Process("P13", t_arrival = 1, t_cpu = 1),
-        Process("P14", t_arrival = 1, t_cpu = 1),
-        Process("P15", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-    ]
-
-    # ps = [
-    #     Process("P1", t_arrival = 3, t_cpu = 2),
-    # ]
-
     visualizer = ProcessPlanifierVisualizer(ps1, FCFS, FCFS.BY_TIME)
     visualizer.plot(verbose=False)
 
-    visualizer = ProcessPlanifierVisualizer(ps2, FCFS, FCFS.BY_TIME)
-    visualizer.plot(verbose=False)
-
-    visualizer = ProcessPlanifierVisualizer(ps3, FCFS, FCFS.BY_TIME)
+    visualizer = ProcessPlanifierVisualizer(ps_priority, FCFS, FCFS.BY_PRIORITY)
     visualizer.plot(verbose=False)
--- processplanifiersimulator.py
+++ processplanifiersimulator.py
@@ -1,10 +1,17 @@
 class ProcessPlanifierSimulator:
-    BY_TIME = lambda p: p.t_arrival
-    BY_PRIORITY = lambda p: p.priority # TODO test
+    BY_TIME = 0
+    BY_PRIORITY = 1
 
-    def __init__(self, processes: list, plan_by=BY_TIME):
+    BY_TIME_FT = lambda p: p.t_arrival
+    BY_PRIORITY_FT = lambda p: p.priority
+
+    def __init__(self, processes: list, plan_by = BY_TIME):
         self.processes = processes
-        self.queue = sorted(self.processes, key=plan_by)
+        self.planning = plan_by
+        self.planning_ft = ProcessPlanifierSimulator.BY_TIME_FT
+        if plan_by == self.BY_PRIORITY:
+            self.planning_ft = ProcessPlanifierSimulator.BY_PRIORITY_FT
+        self.queue = sorted(self.processes, key=self.planning_ft)
         self.t = 0
         self.__ended = False
 
@@ -38,24 +45,24 @@ class FCFS(ProcessPlanifierSimulator):
         if self.ended:
             return self.t
         for process in self.queue:
-            if self.t < process.t_arrival:
+            if self.planning == self.BY_TIME and self.t < process.t_arrival:
                 self.t = process.t_arrival
             self.t = process.run_for(self.t, process.t_cpu)
         self.ended = True
 
-class SJF(ProcessPlanifierSimulator):
-    def __init__(self, processes: list):
-        super().__init__(processes, self.BY_TIME)
+# class SJF(ProcessPlanifierSimulator):
+#     def __init__(self, processes: list):
+#         super().__init__(processes, self.BY_TIME)
 
-    def run(self):
-        pass # TODO
+#     def run(self):
+#         pass # TODO
 
-class SRTF(ProcessPlanifierSimulator):
-    def __init__(self, processes: list):
-        super().__init__(processes, self.BY_TIME)
+# class SRTF(ProcessPlanifierSimulator):
+#     def __init__(self, processes: list):
+#         super().__init__(processes, self.BY_TIME)
 
-    def run(self):
-        pass # TODO
+#     def run(self):
+#         pass # TODO
 
 # class RR(ProcessPlanifierSimulator):
 #     def __init__(self, processes: list):
