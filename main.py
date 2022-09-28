aa0b75ecee0227353f602c9734c1b8d6738d4ec6
Jkutkut <jorgeregonzalez@gmail.com>
2022-09-28 13:24:47 +0200

    Colors final and stable

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from processplanifiervisualizer import ProcessPlanifierVisualizer
 
 if __name__ == "__main__":
     # TODO
-    ps = [
+    ps1 = [
         Process("P1", t_arrival = 1, t_cpu = 1),
         Process("P2", t_arrival = 1, t_cpu = 2),
         Process("P3", t_arrival = 6, t_cpu = 3),
@@ -13,9 +13,10 @@ if __name__ == "__main__":
         Process("P5", t_arrival = 8, t_cpu = 4),
         Process("P6", t_arrival = 9, t_cpu = 3),
         Process("P7", t_arrival = 9, t_cpu = 1),
+        Process("P8", t_arrival = 9, t_cpu = 1),
     ]
 
-    ps = [
+    ps2 = [
         Process("P1",  t_arrival = 0, t_cpu = 1),
         Process("P2",  t_arrival = 1, t_cpu = 2),
         Process("P3",  t_arrival = 1, t_cpu = 3),
@@ -32,22 +33,39 @@ if __name__ == "__main__":
         Process("P14", t_arrival = 1, t_cpu = 1),
         Process("P15", t_arrival = 1, t_cpu = 1),
         Process("P16", t_arrival = 1, t_cpu = 1),
+    ]
+    ps3 = [
+        Process("P1",  t_arrival = 0, t_cpu = 1),
+        Process("P2",  t_arrival = 1, t_cpu = 2),
+        Process("P3",  t_arrival = 1, t_cpu = 3),
+        Process("P4",  t_arrival = 1, t_cpu = 1),
+        Process("P5",  t_arrival = 1, t_cpu = 2),
+        Process("P6",  t_arrival = 1, t_cpu = 1),
+        Process("P7",  t_arrival = 1, t_cpu = 1),
+        Process("P8",  t_arrival = 1, t_cpu = 2),
+        Process("P9",  t_arrival = 1, t_cpu = 1),
+        Process("P10", t_arrival = 1, t_cpu = 1),
+        Process("P11", t_arrival = 1, t_cpu = 3),
+        Process("P12", t_arrival = 1, t_cpu = 1),
+        Process("P13", t_arrival = 1, t_cpu = 1),
+        Process("P14", t_arrival = 1, t_cpu = 1),
+        Process("P15", t_arrival = 1, t_cpu = 1),
         Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
-        Process("P16", t_arrival = 1, t_cpu = 1),
+        Process("P1",  t_arrival = 0, t_cpu = 1),
+        Process("P2",  t_arrival = 1, t_cpu = 2),
+        Process("P3",  t_arrival = 1, t_cpu = 3),
+        Process("P4",  t_arrival = 1, t_cpu = 1),
+        Process("P5",  t_arrival = 1, t_cpu = 2),
+        Process("P6",  t_arrival = 1, t_cpu = 1),
+        Process("P7",  t_arrival = 1, t_cpu = 1),
+        Process("P8",  t_arrival = 1, t_cpu = 2),
+        Process("P9",  t_arrival = 1, t_cpu = 1),
+        Process("P10", t_arrival = 1, t_cpu = 1),
+        Process("P11", t_arrival = 1, t_cpu = 3),
+        Process("P12", t_arrival = 1, t_cpu = 1),
+        Process("P13", t_arrival = 1, t_cpu = 1),
+        Process("P14", t_arrival = 1, t_cpu = 1),
+        Process("P15", t_arrival = 1, t_cpu = 1),
         Process("P16", t_arrival = 1, t_cpu = 1),
     ]
 
@@ -55,5 +73,11 @@ if __name__ == "__main__":
     #     Process("P1", t_arrival = 3, t_cpu = 2),
     # ]
 
-    visualizer = ProcessPlanifierVisualizer(ps, FCFS, FCFS.BY_TIME)
-    visualizer.plot()
+    visualizer = ProcessPlanifierVisualizer(ps1, FCFS, FCFS.BY_TIME)
+    visualizer.plot(verbose=False)
+
+    visualizer = ProcessPlanifierVisualizer(ps2, FCFS, FCFS.BY_TIME)
+    visualizer.plot(verbose=False)
+
+    visualizer = ProcessPlanifierVisualizer(ps3, FCFS, FCFS.BY_TIME)
+    visualizer.plot(verbose=False)
--- processplanifiervisualizer.py
+++ processplanifiervisualizer.py
@@ -5,12 +5,10 @@ from processplanifiersimulator import *
 
 class ProcessPlanifierVisualizer:
     # Colors
-    MIN_COLOR_CODE = 0
-    # MAX_COLOR_CODE = 255
-    # MIN_COLOR_CODE = 105
+    MIN_COLOR_CODE = 16
     MAX_COLOR_CODE = 231
     NC = "\033[0m"
-    MAX_PROCESSES = MAX_COLOR_CODE - MIN_COLOR_CODE
+    MAX_PROCESSES = 32
 
     DX = 5
 
@@ -21,45 +19,6 @@ class ProcessPlanifierVisualizer:
         if len(self.ps) > self.MAX_PROCESSES:
             raise Exception("Too many processes")
         self.simulation = simulator(self.ps, *modifiers)
-
-    def represent(self, verbose: bool = True, unit="ns") -> str:
-        if not self.simulation.ended:
-            self.simulation.run()
-        s = self.represent_processes()
-
-        if verbose:
-            s += f"\nFormulas:\n"
-            s += f"  Time queue: Tq = t_end - t_arrival\n"
-            s += f"  Tq normalized: normalize(Tq) = Tq / t_cpu\n"
-            s += f"  Avg Tq: avg(Tq) = sum(Tq(n)) / n\n\n"
-            s += f"  t_wait = (last start time of process) - (t_cpu consumed previously) - t_arrival\n"
-            s += f"  Avg t_wait: avg(t_wait) = sum(t_wait(n)) / n\n"
-
-        table = [
-            [f" {i}  " for i in ["Process", "t_arrival", "t_cpu", "Tq", "Tq normalized", "t_wait"]]
-        ]
-
-        for id, p in enumerate(self.ps):
-            name = self.colorize_txt(id, p.name.center(len(table[0][0])))
-            t_arrival = self.colorize_txt(id, f"{p.t_arrival}{unit}".center(len(table[0][1])))
-            t_cpu = self.colorize_txt(id, f"{p.t_cpu}{unit}".center(len(table[0][2])))
-            t_queue = self.colorize_txt(id, f"{p.t_queue}{unit}".center(len(table[0][3])))
-            t_queue_normalized = self.colorize_txt(id, f"{p.t_queue_normalized}".center(len(table[0][4])))
-            t_wait = self.colorize_txt(id, f"{p.t_wait}{unit}".center(len(table[0][5])))
-            
-            table.append([
-                name, t_arrival, t_cpu, t_queue, t_queue_normalized, t_wait
-            ])
-        table = '\n'.join([''.join(row) for row in table])
-        s += f"\n{table}\n"
-
-        s = f"{s}\nAvg Time queue: {Process.avg_t_queue(self.ps):.3f}{unit}\n"
-        s = f"{s}Avg Time wait: {Process.avg_t_wait(self.ps):.3f}{unit}\n"
-
-        return s
-
-    def plot(self, unit="ns"):
-        print(self.represent(unit=unit))
 
     def represent_processes(self) -> str:
         t = self.simulation.t
@@ -92,15 +51,61 @@ class ProcessPlanifierVisualizer:
         graph = f"\n{graph}\n       {''.join(legend)}\n"
         return graph
 
+    def represent(self, verbose: bool = True, unit="ns") -> str:
+        if not self.simulation.ended:
+            self.simulation.run()
+        s = self.represent_processes()
+
+        if verbose:
+            s += f"\nFormulas:\n"
+            s += f"  Time queue: Tq = t_end - t_arrival\n"
+            s += f"  Tq normalized: normalize(Tq) = Tq / t_cpu\n"
+            s += f"  Avg Tq: avg(Tq) = sum(Tq(n)) / n\n\n"
+            s += f"  t_wait = (last start time of process) - (t_cpu consumed previously) - t_arrival\n"
+            s += f"  Avg t_wait: avg(t_wait) = sum(t_wait(n)) / n\n"
+
+        table = [
+            [f" {i}  " for i in ["Process", "t_arrival", "t_cpu", "Tq", "Tq normalized", "t_wait"]]
+        ]
+
+        for id, p in enumerate(self.ps):
+            name = self.colorize_txt(id, p.name.center(len(table[0][0])))
+            t_arrival = self.colorize_txt(id, f"{p.t_arrival}{unit}".center(len(table[0][1])))
+            t_cpu = self.colorize_txt(id, f"{p.t_cpu}{unit}".center(len(table[0][2])))
+            t_queue = self.colorize_txt(id, f"{p.t_queue}{unit}".center(len(table[0][3])))
+            t_queue_normalized = self.colorize_txt(id, f"{p.t_queue_normalized:.3f}".center(len(table[0][4])))
+            t_wait = self.colorize_txt(id, f"{p.t_wait}{unit}".center(len(table[0][5])))
+            
+            table.append([
+                name, t_arrival, t_cpu, t_queue, t_queue_normalized, t_wait
+            ])
+        table = '\n'.join([''.join(row) for row in table])
+        s += f"\n{table}\n"
+
+        s = f"{s}\nAvg Time queue: {Process.avg_t_queue(self.ps):.3f}{unit}\n"
+        s = f"{s}Avg Time wait: {Process.avg_t_wait(self.ps):.3f}{unit}\n"
+
+        return s
+
+    def plot(self, verbose: bool = True, unit="ns"):
+        print(self.represent(verbose = verbose, unit=unit))
+
     def colorize_txt(self, color_id: int, txt: str) -> str:
         return f"{self.get_color(color_id)}{txt}{self.NC}"
 
     def get_color(self, index: int):
+        N = len(self.ps)
+        step = 64 if N <= 8 else 4
+
+        dColor = 36
         if index % 2 == 0:
-            color = self.MAX_COLOR_CODE - index * 6
+            color = self.MAX_COLOR_CODE - (index // 2) * step
         else:
-            color = self.MAX_COLOR_CODE - index * 6 - 5
+            color = self.MAX_COLOR_CODE - (index - 1) // 2 * step - 5
+        color -= (index // 12) * dColor
 
         if color < self.MIN_COLOR_CODE:
-            raise Exception("Color not defined")
+            raise Exception(f"Color not defined {color}")
+        if color > self.MAX_COLOR_CODE:
+            raise Exception(f"Color not defined max {color}")
         return f"\033[38;5;{color}m"
