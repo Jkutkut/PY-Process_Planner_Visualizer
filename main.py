30f008fc2393430fa6d18119529fb3f1a7fdaced
jkutkut <jorgeregonzalez@gmail.com>
2022-09-28 15:55:34 +0200

    represent now shows the priority

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ if __name__ == "__main__":
 
     ps1 = [
         Process("P1", t_arrival = 1, t_cpu = 1),
-        Process("P2", t_arrival = 1, t_cpu = 2),
+        Process("P2", t_arrival = 9, t_cpu = 2),
         Process("P3", t_arrival = 6, t_cpu = 3),
-        Process("P4", t_arrival = 7, t_cpu = 5),
+        Process("P4", t_arrival = 3, t_cpu = 5),
         Process("P5", t_arrival = 8, t_cpu = 4),
         Process("P6", t_arrival = 9, t_cpu = 3),
-        Process("P7", t_arrival = 9, t_cpu = 1),
-        Process("P8", t_arrival = 9, t_cpu = 1),
+        Process("P7", t_arrival = 1, t_cpu = 1),
+        Process("P8", t_arrival = 2, t_cpu = 1),
     ]
 
     visualizer = ProcessPlanifierVisualizer(ps1, FCFS, FCFS.BY_TIME)
