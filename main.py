5e95731669ede9bebf9294fe9764b00f60fa1803
Jkutkut <jorgeregonzalez@gmail.com>
2022-09-29 23:52:23 +0200

    main ready

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from process import *
-from processplanifiersimulator import *
-from processplanifiervisualizer import ProcessPlanifierVisualizer
+from processplannersimulator import *
+from processplannervisualizer import ProcessPlannerVisualizer
 
+VERBOSE = False
 
 if __name__ == "__main__":
     ps = [
@@ -14,22 +15,21 @@ if __name__ == "__main__":
         Process("P7", t_arrival = 1, t_cpu = 1),
         Process("P8", t_arrival = 2, t_cpu = 1),
     ]
-    # ps_priority = [
-    #     Process("P1", priority = 1, t_cpu = 1),
-    #     Process("P2", priority = 1, t_cpu = 2),
-    #     Process("P3", priority = 6, t_cpu = 3),
-    #     Process("P4", priority = 7, t_cpu = 5),
-    #     Process("P5", priority = 8, t_cpu = 4),
-    #     Process("P6", priority = 9, t_cpu = 3),
-    #     Process("P7", priority = 9, t_cpu = 1),
-    #     Process("P8", priority = 9, t_cpu = 1),
-    # ]
+    # # ps_priority = [
+    # #     Process("P1", priority = 1, t_cpu = 1),
+    # #     Process("P2", priority = 1, t_cpu = 2),
+    # #     Process("P3", priority = 6, t_cpu = 3),
+    # #     Process("P4", priority = 7, t_cpu = 5),
+    # #     Process("P5", priority = 8, t_cpu = 4),
+    # #     Process("P6", priority = 9, t_cpu = 3),
+    # #     Process("P7", priority = 9, t_cpu = 1),
+    # #     Process("P8", priority = 9, t_cpu = 1),
+    # # ]
 
-    visualizer = ProcessPlanifierVisualizer(ps, FCFS, FCFS.BY_TIME)
-    # visualizer = ProcessPlanifierVisualizer(ps_priority, FCFS, FCFS.BY_PRIORITY)
-    # visualizer = ProcessPlanifierVisualizer(ps, SJF)
-    # visualizer = ProcessPlanifierVisualizer(ps, SRJF)
-    # visualizer = ProcessPlanifierVisualizer(ps, RR, 2)
-    
-    visualizer.plot(verbose=True)
-    # visualizer.plot(verbose=False)
+    visualizer = ProcessPlannerVisualizer(ps, FCFS, FCFS.BY_TIME)
+    # # visualizer = ProcessPlannerVisualizer(ps_priority, FCFS, FCFS.BY_PRIORITY)
+    # # visualizer = ProcessPlannerVisualizer(ps, SJF)
+    # # visualizer = ProcessPlannerVisualizer(ps, SRJF)
+    # # visualizer = ProcessPlannerVisualizer(ps, RR, 2)
+
+    visualizer.plot(verbose=VERBOSE)
