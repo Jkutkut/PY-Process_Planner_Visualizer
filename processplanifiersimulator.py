450e5e76704a2942f1481e4324fc002c0e03150b
jkutkut <jorgeregonzalez@gmail.com>
2022-09-29 18:12:31 +0200

    Simulation logic with planning refactored

--- processplanifiersimulator.py
+++ processplanifiersimulator.py
@@ -1,17 +1,21 @@
 class ProcessPlanifierSimulator:
+    BY_ENTRY = -1
     BY_TIME = 0
     BY_PRIORITY = 1
 
-    BY_TIME_FT = lambda p: p.t_arrival
-    BY_PRIORITY_FT = lambda p: p.priority
+    PLANNING_FTS = [
+        lambda p: p.t_arrival,
+        lambda p: p.priority
+    ]
 
     def __init__(self, processes: list, plan_by = BY_TIME):
         self.processes = processes
+        self.queue = [p for p in processes]
+
         self.planning = plan_by
-        self.planning_ft = ProcessPlanifierSimulator.BY_TIME_FT
-        if plan_by == self.BY_PRIORITY:
-            self.planning_ft = ProcessPlanifierSimulator.BY_PRIORITY_FT
-        self.queue = sorted(self.processes, key=self.planning_ft)
+        if self.planning != self.BY_ENTRY:
+            self.queue = sorted(self.queue, key = self.PLANNING_FTS[self.planning])
+
         self.t = 0
         self.__ended = False
 
@@ -78,7 +82,7 @@ class SJF(ProcessPlanifierSimulator):
                 # Get all with t_remaining lowest
                 lst_s = list(filter(lambda p: p.t_remaining == min_t_remaining, lst_s))
                 if len(lst_s) > 1:
-                    lst_s = sorted(lst_s, key=SJF.BY_TIME_FT) # Apply FCFS (smallest t_arrival)
+                    lst_s = sorted(lst_s, key=SJF.PLANNING_FTS[SJF.BY_TIME]) # Apply FCFS (smallest t_arrival)
             p = lst_s[0]
             self.t = p.run_for(self.t, p.t_cpu)
             self.queue.remove(p)
@@ -86,7 +90,7 @@ class SJF(ProcessPlanifierSimulator):
 
 class SRTF(ProcessPlanifierSimulator):
     def __init__(self, processes: list):
-        super().__init__(processes, self.BY_TIME)
+        super().__init__(processes, self.BY_ENTRY)
 
     def run(self):
         remaining_ft = lambda p: p.t_remaining
