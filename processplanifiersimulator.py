eb3e23d68be3d6dfbb94727c86cc728050e9c7e4
Jkutkut <jorgeregonzalez@gmail.com>
2022-09-29 23:24:46 +0200

    simulation refactored to final version

--- processplanifiersimulator.py
+++ processplanifiersimulator.py
@@ -1,36 +1,50 @@
 class ProcessPlanifierSimulator:
+    '''
+    Class with the logic to simulate a group of processes.
+    '''
+
     BY_ENTRY = -1
     BY_TIME = 0
     BY_PRIORITY = 1
 
-    PLANNING_FTS = [
+    _PLANNING_FTS = [
         lambda p: p.t_arrival,
         lambda p: p.priority
     ]
 
     def __init__(self, processes: list, plan_by = BY_TIME):
-        self.processes = processes
-        self.queue = [p for p in processes]
+        self._processes = processes
+        self._queue = [p for p in processes]
 
-        self.planning = plan_by
-        if self.planning != self.BY_ENTRY:
-            self.queue = sorted(self.queue, key = self.PLANNING_FTS[self.planning])
+        self._planning = plan_by
+        if self._planning != self.BY_ENTRY:
+            self._queue = sorted(self._queue, key = self._PLANNING_FTS[self._planning])
 
-        self.t = 0
+        self._t = 0
         self.__ended = False
+
+    # ******** METHODS ********
 
     def run(self) -> int:
         raise Exception("Not implemented")
 
-    # ************ GETTERS ************
+    def _end(self):
+        self.check_end()
+        self.__ended = True
+
+    # ******** GETTERS ********
+
+    @property
+    def t(self):
+        return self._t
 
     @property
     def ended(self):
         return self.__ended
 
-    def avalible2run(self, t: int) -> list:
+    def _available2run(self, t: int) -> list:
         lst = []
-        for p in self.processes:
+        for p in self._processes:
             if p.ended:
                 continue
             if p.t_arrival > t:
@@ -38,18 +52,14 @@ class ProcessPlanifierSimulator:
             lst.append(p)
         return lst
 
-    # ************ SETTERS ************
-
-    @ended.setter
-    def ended(self, value):
-        self.check_end()
-        self.__ended = value
-
-    # ************ CHECKERS ************
+    # ******** CHECKERS ********
 
     def check_end(self):
-        if any([not process.ended for process in self.processes]):
+        if any([not process.ended for process in self._processes]):
             raise Exception("Not all processes have ended")
+
+
+# ******** Planifiers ********
 
 class FCFS(ProcessPlanifierSimulator):
     def __init__(self, processes: list, plan_by):
@@ -58,20 +68,23 @@ class FCFS(ProcessPlanifierSimulator):
     def run(self) -> int:
         if self.ended:
             return self.t
-        for process in self.queue:
-            if self.planning == self.BY_TIME and self.t < process.t_arrival:
-                self.t = process.t_arrival
-            self.t = process.run_for(self.t, process.t_cpu)
-        self.ended = True
+        for process in self._queue:
+            if self._planning == self.BY_TIME and self.t < process.t_arrival:
+                self._t = process.t_arrival
+            self._t = process.run_for(self.t, process.t_cpu)
+        self._end()
+        return self.t
 
 class SJF(ProcessPlanifierSimulator):
     def __init__(self, processes: list):
         super().__init__(processes, self.BY_TIME)
 
     def run(self):
+        if self.ended:
+            return self.t
         remaining_ft = lambda p: p.t_remaining
-        while len(self.queue) > 0:
-            lst = self.avalible2run(self.t)
+        while len(self._queue) > 0:
+            lst = self._available2run(self.t)
             if len(lst) == 0:
                 t += 1
                 continue
@@ -82,22 +95,25 @@ class SJF(ProcessPlanifierSimulator):
                 # Get all with t_remaining lowest
                 lst_s = list(filter(lambda p: p.t_remaining == min_t_remaining, lst_s))
                 if len(lst_s) > 1:
-                    lst_s = sorted(lst_s, key=SJF.PLANNING_FTS[SJF.BY_TIME]) # Apply FCFS (smallest t_arrival)
+                    lst_s = sorted(lst_s, key=SJF._PLANNING_FTS[SJF.BY_TIME]) # Apply FCFS (smallest t_arrival)
             p = lst_s[0]
-            self.t = p.run_for(self.t, p.t_cpu)
-            self.queue.remove(p)
-        self.ended = True
+            self._t = p.run_for(self.t, p.t_cpu)
+            self._queue.remove(p)
+        self._end()
+        return self.t
 
 class SRTF(ProcessPlanifierSimulator):
     def __init__(self, processes: list):
         super().__init__(processes, self.BY_ENTRY)
 
     def run(self):
+        if self.ended:
+            return self.t
         remaining_ft = lambda p: p.t_remaining
-        while len(self.queue) > 0:
-            lst = self.avalible2run(self.t)
+        while len(self._queue) > 0:
+            lst = self._available2run(self.t)
             if len(lst) == 0:
-                self.t += 1
+                self._t += 1
                 print(self.t)
                 continue
             # Sortest Job Fist (SJF)
@@ -107,12 +123,13 @@ class SRTF(ProcessPlanifierSimulator):
                 # Get all with t_remaining lowest
                 lst_s = list(filter(lambda p: p.t_remaining == min_t_remaining, lst_s))
                 if len(lst_s) > 1:
-                    lst_s = sorted(lst_s, key=SJF.PLANNING_FTS[SJF.BY_TIME]) # Apply FCFS (smallest t_arrival)
+                    lst_s = sorted(lst_s, key=SJF._PLANNING_FTS[SJF.BY_TIME]) # Apply FCFS (smallest t_arrival)
             p = lst_s[0]
-            self.t = p.run_for(self.t, 1)
+            self._t = p.run_for(self.t, 1)
             if p.ended:
-                self.queue.remove(p)
-        self.ended = True
+                self._queue.remove(p)
+        self._end()
+        return self.t
 
 class SRJF(SRTF):
     pass
@@ -123,18 +140,19 @@ class RR(ProcessPlanifierSimulator):
         self.quantum = quantum
 
     def run(self):
-        active_queue = []
+        if self.ended:
+            return self.t
         i = -1
-        while len(self.queue) > 0:
+        while len(self._queue) > 0:
             i += 1
-            lst = self.avalible2run(self.t)
+            lst = self._available2run(self.t)
             if i >= len(lst):
                 i = 0
             p = lst[i]
-            self.t = p.run_for(self.t, self.quantum)
+            self._t = p.run_for(self.t, self.quantum)
             if p.ended:
-                self.queue.remove(p)
+                self._queue.remove(p)
                 lst.remove(p)
                 i -= 1
-            
-        self.ended = True
+        self._end()
+        return self.t
